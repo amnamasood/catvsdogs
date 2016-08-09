ee1a030b3fe08f645b9eac1995b72942ad0d9d50
amnamasood <14beeamasood@seecs.edu.pk>
2016-08-09 17:18:43 +0500

    Update catsvsdogs.py

--- catsvsdogs.py
+++ catsvsdogs.py
@@ -43,32 +43,36 @@ def  load_test():
 data,label = load_data()
 test_data, test_label = load_test()
 x = tf.placeholder(tf.float32, [None, 7500])
-W = tf.Variable(tf.truncated_normal([7500, 2],stddev=1))
-b = tf.Variable(tf.constant(0.1,shape=[2]))
+W = tf.Variable(tf.zeros([7500, 2]))
+b = tf.Variable(tf.zeros([2]))
 #y = tf.nn.softmax(tf.matmul(x, W) + b)
 y = tf.nn.log_softmax(tf.matmul(x, W) + b)
 y_ = tf.placeholder(tf.float32, [None,2])
 #cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 #cross_entropy = -tf.reduce_sum(y_*tf.log(tf.clip_by_value(y,1e-10,1.0)))
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * y, reduction_indices=[1]))
-train_step = tf.train.GradientDescentOptimizer(0.0001).minimize(cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.000001).minimize(cross_entropy)
 init=tf.initialize_all_variables()
 sess=tf.Session()
 sess.run(init)
 j=0
 k=19
-for i in range(20):
-  batch_xs = data[j:k, :]
-  batch_ys = label[j:k,:]
+for i in range(2):
+  #batch_xs = data[j:k, :]
+  #batch_ys = label[j:k,:]
+  batch_xs = data
+  batch_ys = label
   j=j+20
   k=k+20
   sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+  print('loss',sess.run(cross_entropy, feed_dict={x: batch_xs, y_: batch_ys}))
   #print(sess.run(b))
   #print(sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys}))
 #ytest = tf.round(y)
 #check1 = tf.cast(check1,tf.float32)
 #correct_prediction = tf.equal(y_,ytest)
-#print(sess.run(W))
+print('bias',sess.run(b))
+print('weights',sess.run(W))
 ytest = tf.nn.softmax(tf.matmul(x,W)+b)
 correct_prediction = tf.equal(tf.argmax(ytest,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
