379faffb768a8c0ab5390b54174d8a7fc13c523f
amnamasood <14beeamasood@seecs.edu.pk>
2017-04-11 18:31:56 +0500

    Create cnn.py

---
+++ cnn.py
@@ -0,0 +1,119 @@
+import os
+from PIL import Image
+import numpy as np
+import glob
+import math
+import tensorflow as tf
+
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+def conv2d(x, W):
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='VALID')
+
+def max_pool_2x2(x):
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                        strides=[1, 2, 2, 1], padding='VALID')
+
+def make_lab(filenames):
+    n=len(filenames)
+    label=np.zeros((n,2),np.float32)
+    for i,files in enumerate(filenames):
+     if 'cat' in str(files):
+       label[i,0]=1.0
+     else:
+       label[i,1]=1.0
+    return label
+
+
+def input_pipeline(batch_size):
+    files=glob.glob('train/*.jpg')
+    label=make_lab(files)
+    files=tf.convert_to_tensor(files,dtype=tf.string)
+    label=tf.convert_to_tensor(label,dtype=tf.float32)
+    
+    filename_queue = tf.train.slice_input_producer([files,label],shuffle=True)
+    
+    labels=filename_queue[1]
+    image = tf.image.decode_jpeg(tf.read_file(filename_queue[0]), channels=3)
+    image = tf.image.resize_images(image, 28, 28)
+
+    min_after_dequeue = 5
+    capacity = min_after_dequeue + 3 * batch_size
+    image_batch, label_batch = tf.train.batch( [image, labels], batch_size=batch_size, capacity=capacity)
+
+    image_batch=tf.reshape(image_batch,[batch_size,28,28,3])
+    label_batch=tf.reshape(label_batch,[batch_size,2])
+    return image_batch, label_batch
+
+
+x,y_ = input_pipeline(100)
+
+W_conv1 = weight_variable([5, 5, 3, 32])
+#W_conv1 = tf.Variable(tf.truncated_normal([5, 5, 3, 32], stddev=2.0/math.sqrt(float(28*28*3))))
+b_conv1 = bias_variable([32])
+
+h_conv1 = tf.nn.relu(conv2d(x, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+W_conv2 = weight_variable([5, 5, 32, 64])
+#W_conv2 = tf.Variable(tf.truncated_normal([5, 5, 32, 64], stddev=2.0/math.sqrt(float(12*12*32))))
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+W_fc1 = weight_variable([4 * 4 * 64, 100])
+#W_fc1 = tf.Variable(tf.truncated_normal([4*4*64, 100], stddev=2.0/math.sqrt(float(4*4*64))))
+b_fc1 = bias_variable([100])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 4*4*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+W_fc2 = weight_variable([100, 64])
+#W_fc2 = tf.Variable(tf.truncated_normal([100, 64], stddev=2.0/math.sqrt(float(100))))
+b_fc2 = bias_variable([64])
+h_fc2 = tf.nn.relu(tf.matmul(h_fc1,W_fc2) + b_fc2)
+
+W_fc3 = weight_variable([64, 2])
+#W_fc3 = tf.Variable(tf.truncated_normal([64, 2], stddev=2.0/math.sqrt(float(64))))
+b_fc3 = bias_variable([2])
+h_fc3 = tf.matmul(h_fc2,W_fc3) + b_fc3
+y_conv = tf.nn.softmax(h_fc3)
+
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv+1e-10), reduction_indices=[1]))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+saver = tf.train.Saver({'W_conv1':W_conv1,'b_conv1':b_conv1,'W_conv2':W_conv2,'b_conv2':b_conv2,'W_fc1':W_fc1,'b_fc1':b_fc1,'W_fc2':W_fc2,'b_fc2':b_fc2,'W_fc3':W_fc3,'b_fc3':b_fc3})
+
+sess = tf.InteractiveSession()
+sess.run(tf.initialize_all_variables())
+
+# Start input enqueue threads.
+coord = tf.train.Coordinator()
+threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+for i in range(100):
+  #if i%100 == 0:
+  train_accuracy = accuracy.eval()
+  print("step %d, training accuracy %g"%(i, train_accuracy))
+  train_step.run()
+train_accuracy = accuracy.eval()
+print("step %d, training accuracy %g"%(i, train_accuracy))
+
+print(sess.run(b_fc2))
+
+saver.save(sess,'/tmp/model-cnn3.ckpt')
+coord.request_stop()
+
+# Wait for threads to finish.
+coord.join(threads)
+sess.close()
